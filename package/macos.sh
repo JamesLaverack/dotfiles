d3a46ac3fc0a2d44e0b69e9cca33c609c768858a
James Laverack <james@jameslaverack.com>
2020-10-04 23:18:53 +0100

    Add MacOS packages

--- package/macos.sh
+++ package/macos.sh
@@ -3,8 +3,13 @@
 # Setup for MacOS
 
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if ! command -v brew &> /dev/null
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Install utilities and applications
-brew install $(cat brew.txt)
-brew cask install $(cat brew-cask.txt)
+brew install $(cat "${DIR}/brew.txt")
+brew cask install $(cat "${DIR}/brew-cask.txt")

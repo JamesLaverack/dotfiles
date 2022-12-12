63993fa4c042b0a07f512cffca5a970dc714f9b6
James Laverack <james.laverack@jetstack.io>
2022-12-12 10:15:03 +0000

    Vim is easy!

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,6 +21,23 @@ return require('packer').startup(function(use)
   -- My list of plugins --
   ------------------------
   use 'Mofiqul/dracula.nvim'
+  use 'williamboman/mason.nvim'    
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'neovim/nvim-lspconfig' 
+  use 'simrat39/rust-tools.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'hrsh7th/nvim-cmp' 
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
+  use 'hrsh7th/cmp-vsnip'                             
+  use 'hrsh7th/cmp-path'                              
+  use 'hrsh7th/cmp-buffer'                            
+  use 'hrsh7th/vim-vsnip'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 
   -------------------------------
   -- End of my list of plugins --

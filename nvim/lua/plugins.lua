dbbd220cf289128a53af87582ecd869c12c0a72b
James Laverack <james.laverack@jetstack.io>
2022-12-12 18:14:35 +0000

    Back to basics

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,53 +1,15 @@
--- SPDX-License-Identifier: MIT
--- Packer bootstrap code *shamelessly* ripped off from the Packer documentation by Wil Thomason, available under the MIT License
--- https://github.com/wbthomason/packer.nvim
-
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
+-- This requires packer.nvim to be installed. See https://github.com/wbthomason/packer.nvim for installation
+-- instructions but the TL;DR is:
+-- $ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 return require('packer').startup(function(use)
+  -- Manage pakcer.nvim with packer.nvim.
   use 'wbthomason/packer.nvim'
-  ------------------------
-  -- My list of plugins --
-  ------------------------
+  -- Dracula is a visual theme that looks nice IMHO
   use 'Mofiqul/dracula.nvim'
-  use 'williamboman/mason.nvim'    
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig' 
-  use 'simrat39/rust-tools.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'hrsh7th/nvim-cmp' 
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'hrsh7th/cmp-vsnip'                             
-  use 'hrsh7th/cmp-path'                              
-  use 'hrsh7th/cmp-buffer'                            
-  use 'hrsh7th/vim-vsnip'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use 'voldikss/vim-floaterm'
-
-  -------------------------------
-  -- End of my list of plugins --
-  -------------------------------
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+  -- Lualina is a pure-lua implementation of the "statusline" for Vim.
+  use 'nvim-lualine/lualine.nvim'
+  -- which-key explains what mappings do, because I always forget
+  use 'folke/which-key.nvim'
 end)
 

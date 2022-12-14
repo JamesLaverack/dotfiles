49575a97e632b805d4bdfd82d60445afb090ef6d
James Laverack <james.laverack@jetstack.io>
2022-12-14 00:02:37 +0000

    Updates

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -24,5 +24,7 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-nvim-lsp-signature-help'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-buffer'
+	-- lua-snip is a snippets framework, required for cmp
+	use 'L3MON4D3/LuaSnip'
 end)
 

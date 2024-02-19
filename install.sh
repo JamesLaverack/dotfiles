68fbc2cb50f64a473949d754a51e17f68d62f45a
James Laverack <james@laverack.dev>
2024-02-19 03:45:58 +0000

    Update Alacritty Dracula Theme to 9ae0fde

--- install.sh
+++ install.sh
@@ -67,7 +67,7 @@ link "${dotfiles_dir}/gpg-agent.conf" "${HOME}/.gnupg/gpg-agent.conf" "GNUPG Set
 
 mkdir_log "${config_dir}/alacritty"
 link "${dotfiles_dir}/alacritty.yml" "${config_dir}/alacritty/alacritty.yml" "Alacritty Config File"
-link "${dotfiles_dir}/alacritty-dracula-theme.yml" "${config_dir}/alacritty/dracula-theme.yml" "Alacritty 'Dracula' Theme File"
+link "${dotfiles_dir}/alacritty-dracula-theme.toml" "${config_dir}/alacritty/dracula-theme.toml" "Alacritty 'Dracula' Theme File"
 
 link "${dotfiles_dir}/nvim" "${config_dir}/nvim" "Neovim Config Directory"
 

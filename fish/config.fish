879f38294495eb9063f8583fa1ef183498d5b8df
James Laverack <james@laverack.dev>
2024-08-26 05:36:28 +0100

    fish: Port ZSH config to Fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,35 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Language
+set -gx LANG en_GB.UTF-8
+
+# Homebrew
+set -gx HOMEBREW_NO_INSTALL_CLEANUP 1
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
+
+# GPG Agent
+set -gx GPG_TTY $(tty)
+set -e SSH_AGENT_PID
+if command -q gpgconf
+  set SSH_AUTH_SOCK $(gpgconf --list-dir agent-ssh-socket)
 end
+
+# Kubernetes
+abbr -a k kubectl
+
+# Rust
+set rust_bin "$HOME/.cargo/bin"
+if test -d $rust_bin
+  fish_add_path $rust_bin
+end
+
+# Cilium
+if command -q cilium
+  set -gx CILIUM_CLI_MODE helm
+end
+
+if status is-interactive
+  # Neovim
+  abbr -a vim nvim
+  abbr -a vi nvim
+  set -gx EDITOR nvim
+end
+

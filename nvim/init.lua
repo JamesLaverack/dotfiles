4624c4204783dac0e8c6eb9c5b70842353e75216
James Laverack <james@laverack.dev>
2024-11-28 12:35:37 +0000

    neovim: Show file icons in nvim-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -88,6 +88,9 @@ require("lazy").setup({
         },
       },
     },
+    {
+      "nvim-tree/nvim-web-devicons"
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
